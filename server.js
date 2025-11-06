a784f0e6634bbaaf34f6116cfb2a101f9b0d9185
Adigitgeek05 <adigitgeek@gmail.com>
2025-11-07 02:56:37 +0530

    Chnages

--- server.js
+++ server.js
@@ -11,4 +11,6 @@ app.post("/api/data", (req, res) => {
   res.status(200).send({ message: "Data received successfully" });
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
